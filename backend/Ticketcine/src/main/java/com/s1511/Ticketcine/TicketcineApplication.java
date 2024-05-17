473640a5ecce363df466f6196d7f8913dfa0705d
GuillermoDivan <guillermodivan@hotmail.com>
2024-05-17 12:55:01 -0300

    Starting project backend. Folders done

---
+++ backend/Ticketcine/src/main/java/com/s1511/Ticketcine/TicketcineApplication.java
@@ -0,0 +1,13 @@
+package com.s1511.Ticketcine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketcineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketcineApplication.class, args);
+	}
+
+}
---
+++ backend/Ticketcine/src/test/java/com/s1511/Ticketcine/TicketcineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s1511.Ticketcine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketcineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
