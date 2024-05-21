d0ea2b8d009200ac793f9e4e9ac4d842926a0af6
Alex David Frías <adavidfruiz@gmail.com>
2024-05-21 01:02:20 -0500

    feat: seat Repository

--- backend/Ticketcine/src/main/java/com/s1511/Ticketcine/domain/repository/SeatRepository.java
+++ backend/Ticketcine/src/main/java/com/s1511/Ticketcine/domain/repository/SeatRepository.java
@@ -1,6 +1,13 @@
 package com.s1511.Ticketcine.domain.repository;
 
+import com.s1511.Ticketcine.domain.entities.Seat;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface SeatRepository extends JpaRepository {
+import java.util.List;
+
+@Repository
+public interface SeatRepository extends JpaRepository<Seat, Long> {
+
+
 }
