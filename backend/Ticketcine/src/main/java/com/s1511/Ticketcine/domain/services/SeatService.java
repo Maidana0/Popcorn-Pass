59ea338a7b8e11f5465f51239bf48279f51b191f
Alex David Frías <adavidfruiz@gmail.com>
2024-05-21 01:02:34 -0500

    feat: seat service

--- backend/Ticketcine/src/main/java/com/s1511/Ticketcine/domain/services/SeatService.java
+++ backend/Ticketcine/src/main/java/com/s1511/Ticketcine/domain/services/SeatService.java
@@ -1,12 +1,20 @@
 package com.s1511.Ticketcine.domain.services;
 
+import com.s1511.Ticketcine.application.dto.SeatDTO;
 import com.s1511.Ticketcine.domain.entities.Seat;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface SeatService {
 
-    List<Seat> findByUserName(String userName);
-
+    List<SeatDTO> findAllSeats();
+    SeatDTO findSeatById(Long id);
+//    List<Seat> findByUserName(String userName);
+//    List<Seat> findByReserved(boolean reserved);
+//    SeatDTO updateSeat(SeatDTO seatDTO);
+//    boolean reserveSeat(Long seatId, Long userId);
+//    boolean cancelReservation(Long reservationId);
 
 }
