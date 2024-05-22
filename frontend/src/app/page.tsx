197403b5294ec722214b5a3c993a65512bce62a7
Maidana <franco_maidana07@hotmail.com>
2024-05-22 12:31:26 -0300

    - Agregue la carpeta 'bin' al gitignore de backend && en Front la Card NowPlaying

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,20 @@
+import CardNowPlaying from "@/components/molecules/CardNowPlaying";
 
 export default function Home() {
   return (
-    <div>
-        Hola xd
+    <div style={{margin:"2.5rem auto", alignItems: "center", display: "flex", justifyContent: "center", flexWrap:"wrap", gap:"3rem" }}>
+
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <h2 style={{ margin: "20px auto" }}>Now Playing...</h2>
+        <CardNowPlaying />
+      </div>
+
+      <div style={{height:"100vh", width:"1px", backgroundColor:"white"}}></div>
+
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <h2 style={{ margin: "20px auto" }}>Coming Soon...</h2>
+        <CardNowPlaying />
+      </div>
     </div>
   );
 }
