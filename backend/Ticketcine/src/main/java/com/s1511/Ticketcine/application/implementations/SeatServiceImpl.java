0c1d072c86569cc3bf4ae8c5afc3ef0820c0ab0d
Alex David Frías <adavidfruiz@gmail.com>
2024-05-22 13:39:54 -0500

    feat: add seat reservation service Impl

--- backend/Ticketcine/src/main/java/com/s1511/Ticketcine/application/implementations/SeatServiceImpl.java
+++ backend/Ticketcine/src/main/java/com/s1511/Ticketcine/application/implementations/SeatServiceImpl.java
@@ -1,10 +1,9 @@
 package com.s1511.Ticketcine.application.implementations;
 
 import com.s1511.Ticketcine.application.dto.SeatDTO;
+import com.s1511.Ticketcine.application.dto.SeatReservationDTO;
 import com.s1511.Ticketcine.application.mapper.SeatMapper;
-import com.s1511.Ticketcine.domain.entities.Reservation;
 import com.s1511.Ticketcine.domain.entities.Seat;
-import com.s1511.Ticketcine.domain.repository.ReservationRepository;
 import com.s1511.Ticketcine.domain.repository.SeatRepository;
 import com.s1511.Ticketcine.domain.services.SeatService;
 import lombok.RequiredArgsConstructor;
@@ -40,63 +39,24 @@ public class SeatServiceImpl implements SeatService {
         return seatMapper.toDTO(seat);
     }
 
+    @Override
+    @Transactional
+    public Optional<Seat> seatReservation(Long id, SeatReservationDTO seatReservationDTO) {
+        Optional<Seat> seatOptional = seatRepository.findById(id);
 
-//    @Override
-//    public List<Seat> findByUserName(String userName) {
-//        return null;
-//    }
-//
-//
-//    @Override
-//    public List<Seat> findByReserved(boolean reserved) {
-//        return null;
-//    }
-//
-//
-//    public SeatDTO updateSeat(SeatDTO seatDTO) {
-//        try {
-//            Seat seat = seatMapper.toEntity(seatDTO);
-//            Seat updatedSeat = seatRepository.save(seat);
-//            return seatMapper.toDTO(updatedSeat);
-//        } catch (Exception e) {
-//            throw new RuntimeException("Error al actualizar el asiento", e);
-//        }
-//    }
-//
-//    @Override
-//    public boolean reserveSeat(Long seatId, Long userId) {
-//        Optional<Seat> optionalSeat = seatRepository.findById(seatId);
-//        if (optionalSeat.isPresent()) {
-//            Seat seat = optionalSeat.get();
-//            if (!seat.isReserved()) {
-//                seat.setReserved(true);
-//                seatRepository.save(seat);
-//                Reservation reservation = new Reservation();
-//                reservation.setSeatId(seatId);
-//                reservation.setUserId(userId);
-//                reservationRepository.save(reservation);
-//                return true;
-//            }
-//        }
-//        return false;
-//    }
-//
-//    @Override
-//    public boolean cancelReservation(Long reservationId) {
-//        Optional<Reservation> optionalReservation = reservationRepository.findById(reservationId);
-//        if (optionalReservation.isPresent()) {
-//            Reservation reservation = optionalReservation.get();
-//            Long seatId = reservation.getSeatId();
-//            Optional<Seat> optionalSeat = seatRepository.findById(seatId);
-//            if (optionalSeat.isPresent()) {
-//                Seat seat = optionalSeat.get();
-//                seat.setReserved(false);
-//                seatRepository.save(seat);
-//                reservationRepository.delete(reservation);
-//                return true;
-//            }
-//        }
-//        return false;
-//    }
+        if (!seatOptional.isPresent()) {
+            return Optional.empty(); // Asiento no encontrado
+        }
+
+        Seat seat = seatOptional.get();
+        if (seat.isReserved()) {
+            return Optional.empty(); // Asiento ya reservado
+        }
+
+        seat.setReserved(seatReservationDTO.reserved());
+        seatRepository.save(seat);
+        return Optional.of(seat); // Asiento reservado exitosamente
+    }
+
 
 }
