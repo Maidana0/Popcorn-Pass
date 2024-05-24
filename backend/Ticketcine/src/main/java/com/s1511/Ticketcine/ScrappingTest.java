eaf9c2b2c3818bb5c755c73b6577e8f3efba0e01
FrancoNordinelli <imnordi@gmail.com>
2024-05-24 10:28:42 -0300

    dataCinemas sql started

--- backend/Ticketcine/src/main/java/com/s1511/Ticketcine/ScrappingTest.java
+++ backend/Ticketcine/src/main/java/com/s1511/Ticketcine/ScrappingTest.java
@@ -2,6 +2,11 @@ package com.s1511.Ticketcine;
 
 import java.io.IOException;
 
+import com.s1511.Ticketcine.infrastructure.controllers.Scraping;
+
+
+
+//TODO: CAMBIAR DATACINEMA SQL POR SCRAPING JSOUP
 public class ScrappingTest {
 
     public static void main(String[] args) throws IOException {
--- backend/Ticketcine/src/main/java/com/s1511/Ticketcine/infrastructure/controllers/Scraping.java
+++ backend/Ticketcine/src/main/java/com/s1511/Ticketcine/infrastructure/controllers/Scraping.java
@@ -1,9 +1,11 @@
-package com.s1511.Ticketcine;
+package com.s1511.Ticketcine.infrastructure.controllers;
 
 import java.io.IOException;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 
 import com.s1511.Ticketcine.domain.repository.CinemaRepository;
@@ -16,6 +18,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class Scraping {
 
+//TODO: TRAER DATOS CON JSOUP Y GUARDAR EN LA BD. 
     private final CinemaRepository cinemaRepository;
     private final ScreenRepository screenRepository;
 
@@ -23,7 +26,18 @@ public class Scraping {
         String url = "https://www.sinca.gob.ar/DatosBasicosEntidades.aspx?Id=291";
         Document doc = Jsoup.connect(url).get();
 
-        System.out.println(doc.html());
+        Elements table = doc.select(".google-visualization-table-table"); // Cambia el índice según la tabla que necesites
+        
+        System.out.println("Tamaño:" + table.html());
+       
+        Elements rows = table.select("thgoogle-visualization-table-tr-head");
+
+        for (Element row : rows) {
+            System.out.print(row.select("th.google-visualization-table-th gradient unsorted").text());
+           
+            System.out.println();
+}
+
     }
     
 }
