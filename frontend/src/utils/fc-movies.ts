42046072d556b07f01ebe6b08a9148cd29d230dd
Maidana <franco_maidana07@hotmail.com>
2024-05-29 16:13:01 -0300

    - Listando proximos estrenos (recibido del backend) con filtros y paginacion local.

---
+++ frontend/src/utils/fc-movies.ts
@@ -0,0 +1,14 @@
+export const filterMovies = (res: Array<any>) => res.filter(movie =>
+    movie.genre.length > 0 &&
+    movie.image &&
+    new Date(movie.releaseDate) > new Date()
+);
+
+export const convertGenre = (genreArray: Array<string>) => genreArray.join(", ")
+
+export const convertImagePath = (description: string, imagePath: string) => {
+    const onlyPath = description.split("_face");
+    return onlyPath[0] + "_face" + imagePath;
+};
+
+
