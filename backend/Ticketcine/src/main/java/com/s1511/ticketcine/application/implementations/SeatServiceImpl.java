61e42f952396d6225fe05ccfe4afff3ed0e74267
Alex David Frías <adavidfruiz@gmail.com>
2024-05-30 02:16:53 -0500

    feat: add seat reservation service impl

--- backend/Ticketcine/src/main/java/com/s1511/ticketcine/application/implementations/SeatServiceImpl.java
+++ backend/Ticketcine/src/main/java/com/s1511/ticketcine/application/implementations/SeatServiceImpl.java
@@ -1,5 +1,7 @@
 package com.s1511.ticketcine.application.implementations;
 
+import com.s1511.ticketcine.domain.entities.User;
+import com.s1511.ticketcine.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,11 +22,10 @@ import java.util.stream.Collectors;
 public class SeatServiceImpl implements SeatService {
 
     private final SeatRepository seatRepository;
-
+    private final UserRepository userRepository;
     private final SeatMapper seatMapper;
 
     @Override
-    @Transactional(readOnly = true)
     public List<SeatDTO> findAllSeats() {
         List<Seat> seats = seatRepository.findAll();
         return seats.stream()
@@ -33,7 +34,6 @@ public class SeatServiceImpl implements SeatService {
     }
 
     @Override
-    @Transactional(readOnly = true)
     public SeatDTO findSeatById(Long id) {
         Seat seat = seatRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Seat not found"));
@@ -50,14 +50,23 @@ public class SeatServiceImpl implements SeatService {
         }
 
         Seat seat = seatOptional.get();
-        if (seat.isReserved()) {
-            return Optional.empty(); // Asiento ya reservado
+        if (!seat.isReserved()) {
+            return Optional.empty(); // Asiento no reservado
         }
 
-        seat.setReserved(seatReservationDTO.reserved());
-        seatRepository.save(seat);
-        return Optional.of(seat); // Asiento reservado exitosamente
-    }
+        Optional<User> userOptional = userRepository.findById(String.valueOf(seatReservationDTO.userId()));
 
+        if (!userOptional.isPresent()) {
+            return Optional.empty(); // Usuario no encontrado o no activo
+        }
+
+        User user = userOptional.get();
+        seat.setCurrentUser(user);
+        seat.setAvailability(seatReservationDTO.availability());
+        seat.setReserved(false); // Asiento ya no está reservado
+
+        seatRepository.save(seat);
+        return Optional.of(seat); // Estado del asiento actualizado exitosamente
+    }
 
 }
