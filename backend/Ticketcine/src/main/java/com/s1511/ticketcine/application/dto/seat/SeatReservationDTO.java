71d1cbb83dc04b66e8c6358c818e35ceb7c82575
Alex David Frías <adavidfruiz@gmail.com>
2024-05-30 15:52:41 -0500

    refactor: change Long to String

--- backend/Ticketcine/src/main/java/com/s1511/ticketcine/application/dto/seat/SeatReservationDTO.java
+++ backend/Ticketcine/src/main/java/com/s1511/ticketcine/application/dto/seat/SeatReservationDTO.java
@@ -3,7 +3,7 @@ package com.s1511.ticketcine.application.dto.seat;
 import com.s1511.ticketcine.domain.entities.Seat;
 
 public record SeatReservationDTO(
-        Long userId,
+        String userId,
         Seat.Availability availability) {
 
 }
