6b401d67e9992cd2a672133ae24a060606cf1a48
FacundoLB <facuu142@gmail.com>
2024-05-30 18:47:55 -0300

    .

--- backend/Ticketcine/src/main/java/com/s1511/ticketcine/domain/entities/FunctionDetails.java
+++ backend/Ticketcine/src/main/java/com/s1511/ticketcine/domain/entities/FunctionDetails.java
@@ -1,13 +1,9 @@
 package com.s1511.ticketcine.domain.entities;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -25,9 +21,13 @@ public class FunctionDetails {
     private String id;
     private LocalDateTime schedule;
     private String movieName;
+
     @ManyToOne
     @JoinColumn(name = "screen_id", referencedColumnName="id")
     private Screen screen;
+    @OneToMany
+    private List<Seat> functionSeats;
+    private Boolean active;
 
     
 
