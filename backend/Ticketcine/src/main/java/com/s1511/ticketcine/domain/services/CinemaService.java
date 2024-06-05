c0b14e6353267b3aeaf46d2e4219c120ce35b318
FacundoLB <facuu142@gmail.com>
2024-06-05 15:10:23 -0300

    fix todo

--- backend/Ticketcine/src/main/java/com/s1511/ticketcine/domain/services/CinemaService.java
+++ backend/Ticketcine/src/main/java/com/s1511/ticketcine/domain/services/CinemaService.java
@@ -5,12 +5,13 @@ import com.s1511.ticketcine.application.dto.cinema.ReadDtoCinema;
 import com.s1511.ticketcine.application.dto.movie.ReadDtoMovie;
 import com.s1511.ticketcine.domain.entities.Cinema;
 
+import java.util.HashSet;
 import java.util.List;
 
 public interface CinemaService {
 
-List<String> getCinemasCityName();
-List<ReadDtoCinema> getCinemaListByCity(String city);
-List<ReadDtoMovie> getMoviesByCinema(String cinemaId);
+    HashSet<String> getCinemasCityName();
+    List<ReadDtoCinema> getCinemaListByCity(String city);
+    List<ReadDtoMovie> getMoviesByCinema(String cinemaId);
 
 }
